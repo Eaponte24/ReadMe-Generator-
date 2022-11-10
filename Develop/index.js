2e8c968399c45f21d267c83c7b3cc1f9f5b3d654
Ricky Aponte <Rickyapontephotography@gmail.com>
2022-11-09 21:46:30 -0500

    installed the inquirer as well as added the questions to be asked

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,54 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    inquirer
+  .prompt([
+    {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'username',
+      },
+    {
+        type: 'input',
+        message: 'What is your email?',
+        name: 'email',
+      },
+    {
+      type: 'input',
+      message: 'What is a short description of your application?',
+      name: 'description',
+    },
+    {
+      type: 'input',
+      message: 'What are the steps required to install your application?',
+      name: 'installation',
+    },
+    {
+      type: 'input',
+      message: 'What are the instructions on how to use your application?',
+      name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'If others would want to contribute to this application, how would they do so?',
+        name: 'contributing',
+      },
+      {
+        type: 'input',
+        message: 'Can you give examples on how to run your application?',
+        name: 'tests',
+      },
+      {
+        type: 'input',
+        message: 'What License will you be using?',
+        name: 'license',
+      }
+  ])
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
